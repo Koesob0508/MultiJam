dfe4383b4aa5c418a636f95e67c260fc81b1728f
Koesob <epi3706@khu.ac.kr>
2024-06-26 15:52:26 +0900

    Button 클릭 후 사라지게 보완

--- Assets/Scripts/StartButtons.cs
+++ Assets/Scripts/StartButtons.cs
@@ -6,25 +6,29 @@ namespace MultiJam
 {
     public class StartButtons : MonoBehaviour
     {
-        [SerializeField] private Button host;
-        [SerializeField] private Button server;
-        [SerializeField] private Button client;
+        public GameObject panel;
+        public Button host;
+        public Button server;
+        public Button client;
 
         private void Awake()
         {
             host.onClick.AddListener(() =>
             {
                 NetworkManager.Singleton.StartHost();
+                panel.SetActive(false);
             });
 
             server.onClick.AddListener(() =>
             {
                 NetworkManager.Singleton.StartServer();
+                panel.SetActive(false);
             });
 
             client.onClick.AddListener(() =>
             {
                 NetworkManager.Singleton.StartClient();
+                panel.SetActive(false);
             });
         }
     }
